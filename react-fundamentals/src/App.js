f0a9fc3531f1702bda7b5e0c797be7e13466a8a2
Josue Martinez <josue@Josues-MBP.hsd1.in.comcast.net>
2019-04-11 23:37:44 -0400

    update

--- react-fundamentals/src/App.js
+++ react-fundamentals/src/App.js
@@ -5,7 +5,6 @@ import Header from './components/site/Header'
 import Footer from './components/site/Footer'
 import Sidebar from './components/site/Sidebar'
 import FunctionalComponentDemo from './components/concepts/FunctionalComponentDemo'
-
 import {
   BrowserRouter as Router,
 } from 'react-router-dom';
